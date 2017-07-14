19248e8a3767c794a4393b1bea9dac9a5079b77a
Iván Alonso <ivan@neverbot.com>
2017-07-14 19:14:44 +0200

    Compile and install shell script

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+os=${OSTYPE//[0-9.]/}
+
+cd driver/dgd/src
+
+if [[ $os == darwin ]]
+then 
+  sed -i '' '964,970 s/^/\/\//' ./kfun/file.cpp
+  sed -i '' -e 's/# -DSLASHSLASH/-DSLASHSLASH #/g' Makefile
+else
+  sed -i '964,970 s/^/\/\//' ./kfun/file.cpp
+  sed -i -e 's/# -DSLASHSLASH/-DSLASHSLASH #/g' Makefile
+fi
+
+make
+make install
+cd ../../..
+
+if [ ! -f driver/config.dgd ]; 
+then
+  cp driver/config.example.dgd driver/config.dgd
+fi
+
+if [ ! -d driver/dgd/tmp ]; 
+then
+  mkdir driver/dgd/tmp
+fi
