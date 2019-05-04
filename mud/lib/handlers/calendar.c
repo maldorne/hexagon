69d2070224a2ad1723292caeb3c79ef60f9b54a8
Iván Alonso <ivan@neverbot.com>
2019-05-04 23:59:08 +0200

    calendar handler translated

--- mud/lib/handlers/calendar.c
+++ mud/lib/handlers/calendar.c
@@ -1,16 +1,23 @@
-// Handler para el calendario, como apoyo al handler de clima
+
+// calendar handler, helping the weather handler
 //   neverbot 04/2009
 
-// Contiene funciones para saber el dia de la semana de una fecha concreta,
-// asi como para dar nombres al año en el que nos encontramos.
+// functions to know the day of the week in a particular date,
+// and to give name to the years
 
 #include <areas/weather.h>
 #include <areas/calendar.h>
+#include <language.h>
 
 inherit "/lib/core/object.c";
 
+#define MONTH_LENGTHS ({ 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 })
+
 // Lista de años ya nombrados (lo unico que se guarda al salvar el handler)
 mapping year_list;
+
+// implemented in /include/mud/translations.h
+string ordinal(int number);
 
 void create()
 {
@@ -20,38 +27,38 @@ void create()
   restore_object(SAVE_FILENAME, 1);
 }
 
-void setup() 
+void setup()
 {
   // Anticloning
-  if (file_name(this_object()) != "/lib/handlers/calendar") 
+  if (file_name(this_object()) != "/lib/handlers/calendar")
   {
     write("Este objeto no puede ser clonado.\n");
     dest_me();
     return;
   }
-  
+
   reset_get();
   set_name("calendario");
   set_short("Calendario");
   set_long("¡Éste es el controlador del calendario!\n");
   add_alias("controlador");
   add_plural("controladores");
-  
+
   // Move esta redefinida, siempre mueve el objeto a HOME
   move("bing");
 }
 
-int move(mixed dest, varargs mixed messin, mixed messout) 
+int move(mixed dest, varargs mixed messin, mixed messout)
 {
   ::move(HOME, messin, messout);
 }
 
-void save_handler() 
-{ 
-  save_object(SAVE_FILENAME, 1); 
+void save_handler()
+{
+  save_object(SAVE_FILENAME, 1);
 }
 
-void dest_me() 
+void dest_me()
 {
   save_object(SAVE_FILENAME, 1);
   ::dest_me();
@@ -68,10 +75,10 @@ mixed * get_year(int num)
   year = allocate(3);
   name_list = table(CALENDAR_TABLE)->query_name_list();
   adjective_list = table(CALENDAR_TABLE)->query_adjective_list();
-  
+
   if (!undefinedp(year_list[num]))
     return year_list[num];
-  
+
   // Nombre del año
   rand = random(sizeof(name_list)/2) * 2;
   year[POS_YEAR_NAME] = name_list[rand];
@@ -79,14 +86,14 @@ mixed * get_year(int num)
   // Genero del nombre
   gender = name_list[rand+1];
   year[POS_YEAR_GENDER] = gender;
-  
+
   // Adjetivo dependiendo del genero
   rand = random(sizeof(adjective_list)/2) * 2;
   if (gender == 1)
     year[POS_YEAR_ADJECTIVE] = adjective_list[rand];
   else
     year[POS_YEAR_ADJECTIVE] = adjective_list[rand+1];
-  
+
   year_list[num] = year;
 
   save_handler();
@@ -100,7 +107,7 @@ string query_year_name(int num)
   int gender;
 
   gender = 0;
-  
+
   if (!undefinedp(year_list[num]))
     year = year_list[num];
   else
@@ -121,16 +128,16 @@ int query_global_day()
   date_data = handler(WEATHER_HANDLER)->query_date_data();
 
   // return (date_data[4] - 1) * 365 + date_data[1];
-  
+
   // No restamos uno al numero de años porque suponemos que existio un año cero!!!
   return date_data[4] * 365 + date_data[1];
 }
 
-int query_week_day(int num_day) 
-{ 
+int query_week_day(int num_day)
+{
   if (!num_day)
     num_day = query_global_day();
-  return (num_day % 7) + 1; 
+  return (num_day % 7) + 1;
 }
 
 string query_week_day_string(varargs int num_day)
@@ -141,7 +148,7 @@ string query_week_day_string(varargs int num_day)
   return table(CALENDAR_TABLE)->query_week_day_string(num_day);
 }
 
-/* 
+/*
  * We'll try to do a simul_efun to wrap the ctime and make it
  * always return information in spanish, with different modes
  * for real-world time or mud time, neverbot 4/2003
@@ -150,83 +157,83 @@ string query_week_day_string(varargs int num_day)
  *  (takes info from the weather handler, ignoring the time
  *   passed as parameter)
  *
- * Added english formatted date, for ftpd compatibility, 
+ * Added english/international formatted date, for ftpd compatibility,
  *   neverbot 12/04
- * 
+ *
  * Added format with only the time, neverbot 1/06
  */
 
-string convert_day(int num)
+string day(int num)
 {
   switch(num)
   {
-    case 0: return "Domingo";
-    case 1: return "Lunes";
-    case 2: return "Martes";
-    case 3: return "Miércoles";
-    case 4: return "Jueves";
-    case 5: return "Viernes";
+    case 0: return _LANG_CALENDAR_SUNDAY;
+    case 1: return _LANG_CALENDAR_MONDAY;
+    case 2: return _LANG_CALENDAR_TUESDAY;
+    case 3: return _LANG_CALENDAR_WEDNESDAY;
+    case 4: return _LANG_CALENDAR_THURSDAY;
+    case 5: return _LANG_CALENDAR_FRIDAY;
     default:
-    case 6: return "Sábado";
+    case 6: return _LANG_CALENDAR_SATURDAY;
   }
 }
 
-string convert_english_day(int num)
+string intl_day(int num)
 {
   switch(num)
   {
-   case 0: return "Sunday";
-   case 1: return "Monday";
-   case 2: return "Tuesday";
-   case 3: return "Wednesday";
-   case 4: return "Thursday";
-   case 5: return "Friday";
+   case 0: return _LANG_CALENDAR_INTL_SUNDAY;
+   case 1: return _LANG_CALENDAR_INTL_MONDAY;
+   case 2: return _LANG_CALENDAR_INTL_TUESDAY;
+   case 3: return _LANG_CALENDAR_INTL_WEDNESDAY;
+   case 4: return _LANG_CALENDAR_INTL_THURSDAY;
+   case 5: return _LANG_CALENDAR_INTL_FRIDAY;
    default:
-   case 6: return "Saturday";
+   case 6: return _LANG_CALENDAR_INTL_SATURDAY;
   }
 }
 
-string convert_month(int num)
+string month(int num)
 {
  switch(num)
  {
-  case 0: return "Enero";
-  case 1: return "Febrero";
-  case 2: return "Marzo";
-  case 3: return "Abril";
-  case 4: return "Mayo";
-  case 5: return "Junio";
-  case 6: return "Julio";
-  case 7: return "Agosto";
-  case 8: return "Septiembre";
-  case 9: return "Octubre";
-  case 10: return "Noviembre";
+  case 0:  return _LANG_CALENDAR_JANUARY;
+  case 1:  return _LANG_CALENDAR_FEBRUARY;
+  case 2:  return _LANG_CALENDAR_MARCH;
+  case 3:  return _LANG_CALENDAR_APRIL;
+  case 4:  return _LANG_CALENDAR_MAY;
+  case 5:  return _LANG_CALENDAR_JUNE;
+  case 6:  return _LANG_CALENDAR_JULY;
+  case 7:  return _LANG_CALENDAR_AUGUST;
+  case 8:  return _LANG_CALENDAR_SEPTEMBER;
+  case 9:  return _LANG_CALENDAR_OCTOBER;
+  case 10: return _LANG_CALENDAR_NOVEMBER;
   default:
-  case 11: return "Diciembre";
+  case 11: return _LANG_CALENDAR_DECEMBER;
   }
 }
 
-string convert_english_month(int num)
+string intl_month(int num)
 {
  switch(num)
  {
-  case 0: return "Jan";
-  case 1: return "Feb";
-  case 2: return "Mar";
-  case 3: return "Apr";
-  case 4: return "May";
-  case 5: return "Jun";
-  case 6: return "Jul";
-  case 7: return "Aug";
-  case 8: return "Sep";
-  case 9: return "Oct";
-  case 10: return "Nov";
+  case 0:  return _LANG_CALENDAR_INTL_JANUARY;
+  case 1:  return _LANG_CALENDAR_INTL_FEBRUARY;
+  case 2:  return _LANG_CALENDAR_INTL_MARCH;
+  case 3:  return _LANG_CALENDAR_INTL_APRIL;
+  case 4:  return _LANG_CALENDAR_INTL_MAY;
+  case 5:  return _LANG_CALENDAR_INTL_JUNE;
+  case 6:  return _LANG_CALENDAR_INTL_JULY;
+  case 7:  return _LANG_CALENDAR_INTL_AUGUST;
+  case 8:  return _LANG_CALENDAR_INTL_SEPTEMBER;
+  case 9:  return _LANG_CALENDAR_INTL_OCTOBER;
+  case 10: return _LANG_CALENDAR_INTL_NOVEMBER;
   default:
-  case 11: return "Dec";
+  case 11: return _LANG_CALENDAR_INTL_DECEMBER;
   }
 }
 
-string convert_birthday(string str) 
+string convert_birthday(string str)
 {
   // we assume it is 4 characters long 'ddmm'
   int day, month, tot;
@@ -235,76 +242,46 @@ string convert_birthday(string str)
   sscanf(str, "%d", tot);
   day = tot / 100;
   month = tot % 100;
-  /* 
-  switch(day) {
-  case 11:
-  retval = "11th"; break;
-  case 12:
-  retval = "12th"; break;
-  case 13:
-  retval = "13th"; break;
-  default:
-  switch(day%10) {
-  case 1:
-  retval = day+"st"; break;
-  case 2:
-  retval = day+"nd"; break;
-  case 3:
-  retval = day+"rd"; break;
-  default:
-  retval = day+"th";
-  }
-  }
-  */
-  /*
-  day += " de ";
-  */
-  retval = day + " de ";
-  return retval + convert_month(month-1);
-} 
 
-int valid_birthday(string str) 
+  return _LANG_CALENDAR_BIRTHDAY_STRING;
+}
+
+int valid_birthday(string str)
 {
-#define LENGTHS ({ 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 })
-
   int tot, month, day;
 
-  if(strlen(str) != 4) 
+  if(strlen(str) != 4)
     return 0;
-  
-  if(!sscanf(str, "%d", tot)) 
+
+  if(!sscanf(str, "%d", tot))
     return 0;
-  
+
   month = tot % 100;
   day = tot / 100;
-  if(month > 12 || month < 1) 
+  if(month > 12 || month < 1)
     return 0;
-  
-  if(day < 1) 
+
+  if(day < 1)
     return 0;
-  
-  return day <= LENGTHS[month];
+
+  return day <= MONTH_LENGTHS[month];
 } /* valid_birthday() */
 
-string repair_number(int num)
-{
-  string res;
+// pad() now an efun
+// private string pad(int num)
+// {
+//   return (num < 10) ? ("0" + num) : ("" + num);
+// }
 
-  res = "";
-
-  if (num < 10)
-   res = "0" + num;
-  else
-   res = "" + num;
-  return res;
-}
+// static nomask string pad(string str, int size, varargs string char)
+#define PAD(x) pad(""+x, 2, "0")
 
 /*
  * Different modes:
  *  - no flag or flag = 0: reduced datetime for logging:
- *    "Lun 21 Abr 2003 15:58:0"
+ *    "Mon 21 Apr 2003 15:58:0"
  *  - flag = 1: generic real-world time (i.e. for the who command):
- *    "Lunes 21 de Abril de 2003"
+ *    "Monday 21 de Abril de 2003"
  *  - flag = 2: date with 6 characters: ddmmaa
  *  - flag = 3: mud datetime (from weather.c)
  *
@@ -320,34 +297,31 @@ string repair_number(int num)
  */
 string ctime(int time, varargs int flag)
 {
-  mixed * datos;
+  mixed * info;
   string result;
 
   result = "";
-  datos = localtime(time);
+  info = localtime(time);
 
   if (!flag || (flag == 0))
   {
-    result = convert_day(datos[LT_WDAY])[0..2];
-    result += " " + repair_number(datos[LT_MDAY]) + " " +
-              convert_month(datos[LT_MON])[0..2]; 
-    result += " " + datos[LT_YEAR] + " ";
-    result += repair_number(datos[LT_HOUR]) + ":" + 
-              repair_number(datos[LT_MIN]) + ":" +
-              repair_number(datos[LT_SEC]);
+    result = day(info[LT_WDAY])[0..2];
+    result += " " + PAD(info[LT_MDAY]) + " " +
+              month(info[LT_MON])[0..2];
+    result += " " + info[LT_YEAR] + " ";
+    result += PAD(info[LT_HOUR]) + ":" +
+              PAD(info[LT_MIN]) + ":" +
+              PAD(info[LT_SEC]);
   }
   else if (flag == 1)
   {
-    result = convert_day(datos[LT_WDAY]);
-    result += " " + repair_number(datos[LT_MDAY]) + " de " +
-              convert_month(datos[LT_MON]);
-    result += " de " + datos[LT_YEAR];
+    result = _LANG_CALENDAR_LONG_DATETIME;
   }
   else if (flag == 2)
   {
-    result = "" + repair_number(datos[LT_MDAY]) +
-             repair_number(datos[LT_MON]+1);
-    result += ("" + (string)datos[LT_YEAR])[2..3];
+    result = "" + PAD(info[LT_MDAY]) +
+             PAD(info[LT_MON]+1);
+    result += ("" + (string)info[LT_YEAR])[2..3];
   }
   else if (flag == 3)
   {
@@ -355,39 +329,68 @@ string ctime(int time, varargs int flag)
   }
   else if (flag == 4)
   {
-    result = "" + repair_number(datos[LT_MDAY]) + "/" +
-             repair_number(datos[LT_MON]+1) + "/";
-    result += ("" + (string)datos[LT_YEAR])[2..3];
-    result += " " + repair_number(datos[LT_HOUR]) + ":" + 
-              repair_number(datos[LT_MIN]) + ":" +
-              repair_number(datos[LT_SEC]);
+    result = "" + PAD(info[LT_MDAY]) + "/" +
+             PAD(info[LT_MON]+1) + "/";
+    result += ("" + (string)info[LT_YEAR])[2..3];
+    result += " " + PAD(info[LT_HOUR]) + ":" +
+              PAD(info[LT_MIN]) + ":" +
+              PAD(info[LT_SEC]);
   }
   else if (flag == 5)
   {
-    result = convert_day(datos[LT_WDAY]);
-    result += " " + repair_number(datos[LT_MDAY]) + " de " +
-              convert_month(datos[LT_MON]);
-    result += " de " + datos[LT_YEAR];
-    result += " - " + repair_number(datos[LT_HOUR]) + ":" + 
-              repair_number(datos[LT_MIN]) + ":" +
-              repair_number(datos[LT_SEC]);
+    result = day(info[LT_WDAY]);
+    result += " " + PAD(info[LT_MDAY]) + " de " +
+              month(info[LT_MON]);
+    result += " de " + info[LT_YEAR];
+    result += " - " + PAD(info[LT_HOUR]) + ":" +
+              PAD(info[LT_MIN]) + ":" +
+              PAD(info[LT_SEC]);
   }
   else if (flag == 6)
   {
-    result = convert_english_day(datos[LT_WDAY])[0..2] + " " +
-             convert_english_month(datos[LT_MON]);
-    result += " " + repair_number(datos[LT_MDAY]) + " " +
-                    repair_number(datos[LT_HOUR]) + ":" + 
-                    repair_number(datos[LT_MIN]) + ":" +
-                    repair_number(datos[LT_SEC]) + " " + 
-                    repair_number(datos[LT_YEAR]);
+    result = intl_day(info[LT_WDAY])[0..2] + " " +
+             intl_month(info[LT_MON])[0..2];
+    result += " " + PAD(info[LT_MDAY]) + " " +
+                    PAD(info[LT_HOUR]) + ":" +
+                    PAD(info[LT_MIN]) + ":" +
+                    PAD(info[LT_SEC]) + " " +
+                    PAD(info[LT_YEAR]);
   }
   else if (flag == 7)
   {
-    result = repair_number(datos[LT_HOUR]) + ":" + 
-             repair_number(datos[LT_MIN]) + ":" +
-             repair_number(datos[LT_SEC]);
+    result = PAD(info[LT_HOUR]) + ":" +
+             PAD(info[LT_MIN]) + ":" +
+             PAD(info[LT_SEC]);
   }
   return result;
-} 
+}
+
+string seconds_to_time_string(int seconds)
+{
+  string * values;
+  values = ({ });
+
+  if (seconds > 86400)
+    values += ({ sprintf("%d "+_LANG_CALENDAR_DAY+((seconds/86400==1)?"":"s"),
+                          seconds/86400) });
+
+  seconds = seconds % 86400;
+
+  if (seconds > 3600)
+    values += ({ sprintf("%d "+_LANG_CALENDAR_HOUR+((seconds/3600==1)?"":"s"),
+                              seconds/3600) });
+
+  seconds = seconds % 3600;
+
+  if (seconds > 60)
+    values += ({ sprintf("%d "+_LANG_CALENDAR_MINUTE+(((seconds/60)%60==1)?"":"s"),
+                              (seconds/60)%60) });
+
+  seconds = seconds % 60;
+
+  values += ({ sprintf("%d "+_LANG_CALENDAR_SECOND+((seconds%60==1)?"":"s"),
+                        seconds%60) });
+
+  return query_multiple_short(values);
+}
 
