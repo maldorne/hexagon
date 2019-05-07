aaac18ccba088f6b24dba3749b5256ff73aa83e9
Iván Alonso <ivan@neverbot.com>
2019-05-07 18:22:35 +0200

    more cmd fixed and translated

--- mud/lib/cmds/coder/more.c
+++ mud/lib/cmds/coder/more.c
@@ -12,21 +12,23 @@ void setup()
   position = 1;
 }
 
-static int cmd(string str, object me, string verb) 
+static int cmd(string str, object me, string verb)
 {
-  object * things;
+  object * things, user;
   string * filenames;
   int egg;
   string spam;
 
   if (!strlen(str))
   {
-    write("Uso: more <fichero>\n");
+    write("Syntax: more <file>\n");
     return 1;
   }
 
-  /* dodgy idea, but allows 'ed here' or 'ed strawberry' */    
-  if (sizeof(things = me->query_role()->wiz_present(str, me))) 
+  user = me->user();
+
+  /* dodgy idea, but allows 'ed here' or 'ed strawberry' */
+  if (sizeof(things = user->query_role()->wiz_present(str, me)))
   {
     spam = object_name(things[0]);
     sscanf(spam, "%s#%d", spam, egg);
@@ -34,54 +36,54 @@ static int cmd(string str, object me, string verb)
       filenames = ({ spam + ".c" });
     else
       filenames = ({ spam });
-  } 
+  }
   else
     filenames = get_files(str);
 
-  if (!sizeof(filenames)) 
-  { 
-    str = get_path(str);
-  } 
-  else 
+  if (!sizeof(filenames))
   {
-    if (sizeof(filenames) > 0) 
+    str = get_path(str);
+  }
+  else
+  {
+    if (sizeof(filenames) > 0)
     {
       str = filenames[0];
-      if (sizeof(filenames) > 1) 
+      if (sizeof(filenames) > 1)
       {
         int loop;
         loop = 0;
         while (loop < sizeof(filenames) && file_size(filenames[loop]) < 0)
           loop++;
-        if (loop >= sizeof(filenames)) 
+        if (loop >= sizeof(filenames))
         {
-          write("No existe ese archivo.\n");
+          write("That file does not exist.\n");
           return 0;
         }
-        else 
+        else
         {
           str = filenames[loop];
-        } 
-        write("Nombre ambiguo, usando: " + str + "\n");
+        }
+        write("Ambiguous name, using: " + str + "\n");
       }
     }
   }
 
-  if (file_size(str) == -2) 
+  if (file_size(str) == -2)
   {
-    write("Es un directorio.\n");
+    write("Is a directory.\n");
     return 1;
   }
 
-  if (file_size(str) == -1) 
+  if (file_size(str) == -1)
   {
-    write("No hay archivos coincidentes.\n");
+    write("There are no matching files.\n");
     return 1;
   }
-  
+
   if (!SECURE->valid_read(str, this_player()))
   {
-    write("No tienes permisos para ver este fichero.\n");
+    write("You have no permissions to see that file.\n");
     return 1;
   }
 
@@ -99,11 +101,11 @@ int ex_spool(string yn, string fil, int linum)
   if (sscanf(yn, "%d", tnum) == 1)
   {
     linum = (int)tnum;
-    if (linum < 1 ) 
+    if (linum < 1 )
       linum = 1;
   }
 
-  if ( (yn == "Q") || (yn == "q") ) 
+  if ( (yn == "Q") || (yn == "q") )
     return 1;
 
   // for (i = 0; i < this_player()->query_rows(); linum++)
@@ -119,7 +121,7 @@ int ex_spool(string yn, string fil, int linum)
   //   write(sprintf("%4d: %s", linum, s1));
   // }
 
-  s1 = read_file_line(fil, linum, this_player()->query_rows());
+  s1 = read_file_line(fil, linum, this_user()->query_rows());
 
   if ( !strlen(s1) )
   {
@@ -132,7 +134,7 @@ int ex_spool(string yn, string fil, int linum)
   for (i = 0; i < sizeof(lines); i++, linum++)
     write(sprintf("%4d: %s\n", linum, lines[i]));
 
-  write(sprintf("Archivo: %s. Q para salir ", fil));
+  write(sprintf("File: %s. Q to quit ", fil));
   input_to("ex_spool", 0, fil, linum);
   return 1;
 }
