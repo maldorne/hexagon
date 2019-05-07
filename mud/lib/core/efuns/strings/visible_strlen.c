cf6e70b7478ebd46fb50349cadf20912c50d254c
Iván Alonso <ivan@neverbot.com>
2019-05-07 16:03:53 +0200

    visible_strlen() efun working at last!! (only with ansi terminals)

--- mud/lib/core/efuns/strings/visible_strlen.c
+++ mud/lib/core/efuns/strings/visible_strlen.c
@@ -1,16 +1,30 @@
 
 static int visible_strlen(string str)
 {
-  int i, result;
+  int i, result, length;
 
   result = 0;
+  length = strlen(str);
 
-  for (i = 0; i < strlen(str); i++)
+  // if we are not using an ansi terminal... well...
+  // visible_strlen won't work
+  if (this_user() && this_user()->query_term_name() != "ansi")
+    return length;
+
+  for (i = 0; i < length; i++)
   {
-    // if (((str[i] <= 97) && (str[i] < 122)) ||
-    //     ((str[i] <= 65) && (str[i] < 90)) )
-    if (str[i] >= 32)
-      result++;
+    // take a look to /lib/handlers/terminal.c for more info
+    // start of ansi control characters
+    if (str[i] == 27)
+    {
+      // advance until the end of the control characters
+      while ((str[i] != 'm') && (i < length))
+        i++;
+
+      continue;
+    }
+
+    result++;
   }
 
   return result;
