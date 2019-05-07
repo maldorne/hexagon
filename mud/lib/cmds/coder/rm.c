ed73cfaca3b85cf590e3640a227e728de0c1abfe
Iván Alonso <ivan@neverbot.com>
2019-05-07 14:17:50 +0200

    rm cmd fixed and translated

--- mud/lib/cmds/coder/rm.c
+++ mud/lib/cmds/coder/rm.c
@@ -8,62 +8,65 @@ void setup()
   position = 1;
 }
 
-static int cmd(string str, object me, string verb) 
+static int cmd(string str, object me, string verb)
 {
     int fsize, loop;
     string *filenames;
 
-    if (this_player(1) != this_player()) return 0;
+    if (this_player(1) != this_user())
+      return 0;
 
-    if (!strlen(str)) 
+    if (!strlen(str))
     {
-      notify_fail("Sintaxis: rm <archivo> [archivo ...]\n");
+      notify_fail("Syntax: rm <file> [file ...]\n");
       return 0;
     }
 
     filenames = get_files(str);
 
-    if (!sizeof(filenames)) 
+    if (!sizeof(filenames))
     {
-      notify_fail("No existe el archivo: '" + str + "'.\n");
+      notify_fail("The file '" + str + "' does not exist.\n");
       return 0;
     }
 
-    for(loop = 0; loop < sizeof(filenames); loop++) 
+    for(loop = 0; loop < sizeof(filenames); loop++)
     {
       string temp, temp2;
 
       temp2 = "";
       str = filenames[loop];
-      
-      if((sscanf(str, "%s/.%s", temp, temp2) && (temp2 == "")) || 
-          sscanf(str, "%s/..%s", temp, temp2) && (temp2 == "") ) 
+
+      if((sscanf(str, "%s/.%s", temp, temp2) && (temp2 == "")) ||
+          sscanf(str, "%s/..%s", temp, temp2) && (temp2 == "") )
       {
         continue;
       }
-      
+
       fsize = file_size(str);
-      
-      if (fsize == -1) 
+
+      if (fsize == -1)
       {
-         notify_fail("No existe el archivo o directorio.\n");
+         notify_fail("File or directory does not exist.\n");
          return 0;
       }
-      if (fsize == -2) 
+
+      if (fsize == -2)
       {
-        if (!rmdir(str)) 
+        if (!rmdir(str))
         {
-          notify_fail("No se puede borrar el directorio: '" + str + "'.\n");
+          notify_fail("Unable to remove directory: '" + str + "'.\n");
           return 0;
         }
-      } 
-      else if (!rm(str)) 
+      }
+      else if (!rm(str))
       {
-        notify_fail("No se puede borrar el archivo.\n");
+        notify_fail("Unable to remove the file.\n");
         return 0;
       }
     }
+
     write("Ok.\n");
     return 1;
 } /* rm_files() */
- 
+
