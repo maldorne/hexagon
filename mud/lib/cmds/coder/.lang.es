4944309aef759a72600df1c4090f2d8cffc4d64a
Iván Alonso <ivan@neverbot.com>
2019-09-03 15:58:30 +0200

    cmd: compare

--- mud/lib/cmds/coder/.lang.es
+++ mud/lib/cmds/coder/.lang.es
@@ -35,3 +35,15 @@
         ((string)ob->query_short() ? (string)ob->query_short() : "objeto") + \
         " en otra dimensión.\n"
 #define _LANG_CLONE_ERROR "Error, no se ha podido clonar el objeto.\n"
+
+// compare
+
+#define _LANG_COMPARE_HELP "Permite comparar dos archivos. " + \
+      "El comando devuelve las lineas de file1 que no se encuentran en " + \
+      "file2 si which_file es igual a 1, y viceversa si which_file es igual a 2.\n\n" + \
+      "Nota: ambas diferencias de file1 y file2 serán devueltas si which_file no " + \
+      "se especifica."
+#define _LANG_COMPARE_SYNTAX "Sintaxis: compare -<which_file> file1 file2\n"
+#define _LANG_COMPARE_INVALID_WHICH "Valor inválido para -<which_file>.\n\nOpciones:\n" + \
+      "-1 para file1\n-2 para file2\n-3 o ningún valor para ambos\n"
+#define _LANG_COMPARE_NOT_A_FILE "No es un archivo: "
--- mud/lib/cmds/coder/compare.c
+++ mud/lib/cmds/coder/compare.c
@@ -2,6 +2,7 @@
 /** coded by Raskolnikov July 1996 **/
 
 #include <mud/cmd.h>
+#include <language.h>
 
 inherit CMD_BASE;
 
@@ -10,64 +11,62 @@ void setup()
   position = 1;
 }
 
-static int cmd(string str, object me, string verb) 
+static int cmd(string str, object me, string verb)
 {
+  object user;
   int i, which_file, check;
   mixed check1, check2;
-  string file1, file2, *filename1, *filename2, 
+  string file1, file2, *filename1, *filename2,
   *text1, *text2, *diffs1, *diffs2, output1, output2;
 
-  if (!strlen(str)) 
+  if (!strlen(str))
   {
-    notify_fail("Usage : compare -<which_file> file1 file2\n");
+    notify_fail(_LANG_COMPARE_SYNTAX);
     return 0;
   }
 
   check = sscanf(str, "-%d %s %s", which_file, file1, file2);
 
-  if (check != 3) 
+  if (check != 3)
   {
     which_file = 3;
     check = sscanf(str, "%s %s", file1, file2);
-    if (!check || check != 2) 
+    if (!check || check != 2)
     {
-      notify_fail("Usage : compare -<which_file> file1 file2\n");
+      notify_fail(_LANG_COMPARE_SYNTAX);
       return 0;
     }
   }
-  if ((which_file < 1) || (which_file > 3)) 
+
+  if ((which_file < 1) || (which_file > 3))
   {
-    notify_fail("Invalid -<which_file> value.\n\nOptions:\n"+
-      "-1 for file1\n-2 for file2\n-3 or nothing for both\n");
+    notify_fail(_LANG_COMPARE_INVALID_WHICH);
     return 0;
   }
+
+  user = me->user();
 
   filename1 = get_files(file1);
   filename2 = get_files(file2);
 
-  if (!sizeof(filename1)) 
+  if (!sizeof(filename1) || !sizeof(filename2))
   {
-    notify_fail("Invalid file1.\n");
-    return 0;
-  }
-
-  if (!sizeof(filename2)) 
-  {
-    notify_fail("Invalid file2.\n");
+    notify_fail(_LANG_CMD_FILE_DOES_NOT_EXIST);
     return 0;
   }
 
   check1 = read_file(filename1[0]);
   check2 = read_file(filename2[0]);
 
-  if (!check1) 
+  if (!check1)
   {
-    notify_fail("Not a file : "+filename1[0]+"\n");
+    notify_fail(_LANG_COMPARE_NOT_A_FILE + filename1[0]+"\n");
     return 0;
   }
-  if (!check2) 
+
+  if (!check2)
   {
-    notify_fail("Not a file : "+filename2[0]+"\n");
+    notify_fail(_LANG_COMPARE_NOT_A_FILE + filename2[0]+"\n");
     return 0;
   }
 
@@ -77,53 +76,45 @@ static int cmd(string str, object me, string verb)
   text2 = explode(check2, "\n");
 
   if ((which_file == 1) || (which_file == 3))
-    for(i=0;i<sizeof(text1);i++)
+    for (i = 0; i < sizeof(text1); i++)
       if (member_array(text1[i], text2) == -1)
         diffs1 += ({ (i+1)+"  "+text1[i] });
 
   if ((which_file == 2) || (which_file == 3))
-    for(i=0;i<sizeof(text2);i++)
+    for (i = 0; i < sizeof(text2); i++)
       if (member_array(text2[i], text1) == -1)
         diffs2 += ({ (i+1)+"  "+text2[i] });
 
   output1 = implode(diffs1, "\n");
   output2 = implode(diffs2, "\n");
 
-  if (which_file == 1) 
+  if (which_file == 1)
   {
-    me->more_string("Lines in "+filename1[0]+" not "+
+    user->more_string("Lines in "+filename1[0]+" not "+
       "in "+filename2[0]+"\n\n"+output1+"\n\n");
     return 1;
   }
 
-  if (which_file == 2) 
+  if (which_file == 2)
   {
-    me->more_string("Lines in "+filename2[0]+" not "+
+    user->more_string("Lines in "+filename2[0]+" not "+
       "in "+filename1[0]+"\n\n"+output2+"\n\n");
     return 1;
   }
 
-  me->more_string("Lines in "+filename1[0]+" not "+
+  user->more_string("Lines in "+filename1[0]+" not "+
     "in "+filename2[0]+"\n\n"+output1+"\n\nLines "+
     "in "+filename2[0]+" not in "+filename1[0]+"\n\n"+
     ""+output2+"\n");
   return 1;
 } /* compare_file() */
 
-string query_short_help() 
+string query_short_help()
 {
-    return "Compara dos archivos y devuelve las diferencias.";
-    /*
-  return "This command allows the user to compare two files.  "
-  "The command returns the lines of file1 that were "
-  "not found in file2 if which_file is equal to 1 and "
-  "vice-versa if which_file is equal to 2.\n\n"
-  "Note: both file1 and file2 differences are returned if "
-  "which_file is not entered.";
-    */
+    return _LANG_COMPARE_HELP;
 }
 
-string query_usage() 
+string query_usage()
 {
-  return "compare -<which_file> file1 file2";
+  return _LANG_COMPARE_SYNTAX;
 }
