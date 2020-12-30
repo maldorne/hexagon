764ce5d4b6e244432633e7047c86e19ad398e2e1
neverbot <ivan@neverbot.com>
2020-12-30 17:23:19 +0100

    feat: void room improved and translated

--- mudlib/lib/areas/void.c
+++ mudlib/lib/areas/void.c
@@ -6,63 +6,68 @@
 // Translated for CcMud, neverbot 7/03
 // Player now go to ENTRY_POS
 
-#define ENTRY_POS "/game/areas/ciudad-capital/barrio-antiguo/rooms/calzada02"
+// TODO this should be a different destination for every game
+// should check the character's game and find it in its master.c
+#define ENTRY_POS "/games/ciudad-capital/areas/ciudad-capital/barrio-antiguo/rooms/calzada02"
 
+#include <language.h>
 #include <areas/common.h>
 
 inherit "/lib/room";
 
 int do_update(string str)
 {
-   int i;
-   object *all;
-   all = all_inventory(this_object());
-   // if(!stringp(str) || str == "here")
-   // {
-      // load destinations
-      CODER_COMMON_ROOM->BAH();
-      ENTRY_POS->BAH();
+  int i;
+  object *all, where;
 
-      for(i = 0; i < sizeof(all); i++)
-         if(interactive(all[i])) 
-         {
-            if(!all[i]->query_creator())
-            {
-               tell_object(all[i], "Sacándote del infierno...\n");
-               all[i]->move(ENTRY_POS);
-               all[i]->do_look();
-            }
+  all = all_inventory(this_object());
+  // if (!stringp(str) || str == "here")
+  // {
+    for (i = 0; i < sizeof(all); i++)
+    {
+      if (interactive(all[i])) 
+      {
+        if (all[i]->query_coder())
+        {
+          tell_object(all[i], _LANG_VOID_MOVING_CREATOR);
+          where = load_object(CODER_COMMON_ROOM);
+        }
+        else
+        {
+          tell_object(all[i], _LANG_VOID_MOVING_PLAYER);
+          where = load_object(ENTRY_POS);
+        }
 
-            if(all[i]->query_creator())
-            {
-               tell_object(all[i],"Viajando hasta la habitación común...\n");
-               all[i]->move(CODER_COMMON_ROOM);
-               all[i]->do_look();
-               tell_room(find_object(CODER_COMMON_ROOM), all[i]->query_cap_name()+
-                  " llega con cara de cansancio... \n", all[i]);
-            }
-         }
-         else
-            all[i]->dest_me();     // dest those NPCs and items
-   // }
-   return 1; 
+        if (!where)
+        {
+          tell_object(all[i], _LANG_VOID_ERROR);
+          return 1;
+        }
+
+        all[i]->move(where);
+        all[i]->do_look();
+      }
+      else
+      {
+        // dest those NPCs and items
+        all[i]->dest_me();
+      }
+    }
+  // }
+  return 1; 
 }
 
 void setup() {
-     set_light(40);
-     set_short(mud_name()+": El Vacío");
-     set_long("Éste es el lugar al que vienes cuando el lugar en el que " +
-              "estás desaparece y no tienes ningún lugar al que ir. " +
-              "La razón por la que estás en él se deberá muy probablemente " +
-              "a algún fallo involuntario del programador de la zona en la " +
-              "que estabas.\n");
+  set_light(40);
+  set_short(_LANG_VOID_SHORT);
+  set_long(_LANG_VOID_LONG);
 
-     add_exit("ciudad", ENTRY_POS, "standard");
+  add_exit(_LANG_VOID_EXIT_NAME, ENTRY_POS, "standard");
 }
 
 void init()
 {
-   add_action("do_update", ({"update", "up"}));
-   ::init();
+  add_action("do_update", _LANG_VOID_COMMANDS);
+  ::init();
 }
 
