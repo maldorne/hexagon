861aca9571bf1598abc1dc33366d440c32c256dc
neverbot <ivan@neverbot.com>
2020-12-31 20:30:30 +0100

    feat: get action translated (mostly)

--- mudlib/lib/living/.lang.es.h
+++ mudlib/lib/living/.lang.es.h
@@ -3,3 +3,33 @@
 
 #define _LANG_DIDNT_WORK "El intento de hacer '%^RED%^" + \
                             curr_act + "%^RESET%^' no funcionó.\n"
+
+// handle
+
+#define _LANG_HANDLE_WAIT_LOADING "Tienes que esperar a que todo tu equipo esté cargado.\n"
+#define _LANG_HANDLE_DEAD "Tu estado etéreo tiene poco efecto en el mundo material.\n "
+#define _LANG_HANDLE_STILL "Aún estas ocupado con la carga.\n"
+#define _LANG_HANDLE_FAIL "No puedes "+query_verb()+" "+query_multiple_short(fail, 1)+".\n"
+                            
+#define _LANG_HANDLE_GET_VERBS ({ "coger" })
+#define _LANG_HANDLE_GIVE_VERBS ({ "poner", "dar" })
+#define _LANG_HANDLE_DROP_VERBS ({ "dejar" })
+
+#define _LANG_HANDLE_GET_SYNTAX "Sintaxis: "+query_verb()+" <objeto> [de <objeto>]\n"
+#define _LANG_HANDLE_GET_PREP "de"
+#define _LANG_HANDLE_GET_CANNOT "No encuentras ningún "+s2+" por aquí.\n"
+#define _LANG_HANDLE_GET_NOTHING "Nada que "+query_verb()+".\n"
+#define _LANG_HANDLE_GET_MESSAGES ({ "Coges", "Coges con un poco de dificultad", \
+          "Te las arreglas como puedes para coger", \
+          "Te resulta muy difícil coger", \
+          "Usas toda tu fuerza y a duras penas logras coger" })
+#define _LANG_HANDLE_GET_MESSAGES_OTHERS ({ "coge", "coge con un poco de dificultad", \
+            "se las arregla como puede para coger", \
+            "tiene muchas dificultades para coger", \
+            "usa toda su fuerza y a duras penas logra coger" })
+
+#define _LANG_HANDLE_GIVE_SYNTAX "Sintaxis: "+query_verb()+" <objeto> <a/en> <objeto>\n"
+#define _LANG_HANDLE_PUT_VERB "poner"
+#define _LANG_HANDLE_PUT_PREP "en"
+#define _LANG_HANDLE_GIVE_PREP "a"
+#define _LANG_HANDLE_GIVE_CANNOT "No se puede encontrar "+person+".\n"
--- mudlib/lib/living/handle.c
+++ mudlib/lib/living/handle.c
@@ -2,10 +2,11 @@
  * has all the give/get stuff.
  * Fixed so that the objects are unworn/unheld..
  * Baldrick..
- * Revisado para Cc, neverbot 4/03
- * Añadido consentimiento para dar objetos, neverbot 05/09
+ * Reviewed for CcMud, neverbot 4/03
+ * added consent to receive items, neverbot 05/09
  */
 
+#include <language.h>
 #include <basic/move.h>
 #include <common/properties.h>
 #include <basic/money.h>
@@ -16,28 +17,31 @@
 
 void handle_commands()
 {
-  // add_action("take", ({ "get", "take", }));
-  add_action("take", "coger");
-
-  // add_action("give", "put");
-  add_action("give", ({ "poner", "dar", }));
-
-  // add_action("drop_ob", "drop");
-  add_action("drop_ob", "dejar");
+  add_action("do_take", _LANG_HANDLE_GET_VERBS);
+  add_action("do_give", _LANG_HANDLE_GIVE_VERBS);
+  add_action("do_drop", _LANG_HANDLE_DROP_VERBS);
 }
 
 void create()
 {
 }
 
-/* this will work as give and put...
- */
-int give(string str, varargs string verb, object *bing, string bing2, int blue)
+// this will work as give and put...
+int do_give(string str, varargs string verb, object *bing, string bing2, int blue)
 {
   string person, thing, sh, con;
   int i, j, tot, max;
-
   object *obs, *per, *ret, *fail;
+
+  i = 0; j = 0;
+
+  // Added to get rid of one more auto_load bug..
+  // Baldrick, nov '94
+  if (this_object()->query_loading() || this_object()->query_property(LOADING_PROP))
+  {
+    notify_fail(_LANG_HANDLE_WAIT_LOADING);
+    return 0;
+  }
 
   if (this_object()->query_timed_property_exists(PASSED_OUT_PROP))
   {
@@ -47,14 +51,15 @@ int give(string str, varargs string verb, object *bing, string bing2, int blue)
 
   if (!strlen(str) && !verb)
   {
-    notify_fail("Sintaxis: "+query_verb()+" <objeto> <a/en> <objeto>\n");
+    notify_fail(_LANG_HANDLE_GIVE_SYNTAX);
     return 0;
   }
 
-  if (query_verb() == "poner")
-    con = " en ";
+  if (query_verb() == _LANG_HANDLE_PUT_VERB)
+    con = " " + _LANG_HANDLE_PUT_PREP + " ";
   else
-    con = " a ";
+    con = " " + _LANG_HANDLE_GIVE_PREP + " ";
+
   if (bing)
   {
     obs = bing;
@@ -64,22 +69,26 @@ int give(string str, varargs string verb, object *bing, string bing2, int blue)
   else
   {
     if (sscanf(str, "%s a %s", thing, person) != 2)
+    {
       if (sscanf(str, "%s en %s", thing, person) != 2)
       {
-        notify_fail("Sintaxis: "+query_verb()+" <objeto> <a/en> <objeto>\n");
+        notify_fail(_LANG_HANDLE_GIVE_SYNTAX);
         return 0;
       }
-      obs = find_match(person, ({ this_object(), environment() }), 1);
-      if (!sizeof(obs))
-      {
-        // notify_fail("No puedo encontrar "+person+" para "+query_verb()+"le nada.\n");
-        notify_fail("No se puede encontrar "+person+".\n");
-        return 0;
-      }
+    }
+
+    obs = find_match(person, ({ this_object(), environment() }), 1);
+
+    if (!sizeof(obs))
+    {
+      notify_fail(_LANG_HANDLE_GIVE_CANNOT);
+      return 0;
+    }
+
     per = obs;
   }
 
-  /* should check to see that we can give things to this person/thing... */
+  // should check to see that we can give things to this person/thing...
   if (!per)
     per = ({ });
 
@@ -88,15 +97,17 @@ int give(string str, varargs string verb, object *bing, string bing2, int blue)
     if (per[j] == this_object())
       continue;
 
-  if (per[j]->query_player() || per[j]->query_npc())
-    if (!per[j]->query_consent(GIVE_CONS))
+    if (per[j]->query_player() || per[j]->query_npc())
     {
-      tell_object(this_object(), per[j]->query_cap_name() +
-        " no quiere que le den nada.\n");
-      tell_object(per[j], this_object()->query_cap_name() +
-        " ha intentado darte algo, pero no te interesa aceptar cosas.\n" +
-        "\t(escribe 'consentir dar sí' si quieres cambiarlo).\n");
-      continue;
+      if (!per[j]->query_consent(GIVE_CONS))
+      {
+        tell_object(this_object(), per[j]->query_cap_name() +
+          " no quiere que le den nada.\n");
+        tell_object(per[j], this_object()->query_cap_name() +
+          " ha intentado darte algo, pero no te interesa aceptar cosas.\n" +
+          "\t(escribe 'consentir dar sí' si quieres cambiarlo).\n");
+        continue;
+      }
     }
 
     obs = find_match(thing, this_object(), 1);
@@ -194,11 +205,20 @@ int give(string str, varargs string verb, object *bing, string bing2, int blue)
   return 1;
 }
 
-int drop_ob(string str)
+int do_drop(string str)
 {
   int i, num;
   object *ob, *ret, *fail;
   string sh;
+
+  // Added to get rid of one more auto_load bug..
+  // Baldrick, nov '94
+  if (this_object()->query_loading() || this_object()->query_property(LOADING_PROP))
+  {
+    notify_fail(_LANG_HANDLE_WAIT_LOADING);
+    return 0;
+  }
+
 
   if (this_object()->query_timed_property_exists(PASSED_OUT_PROP))
   {
@@ -209,13 +229,6 @@ int drop_ob(string str)
   if (!strlen(str))
   {
     notify_fail("Sintaxis: "+query_verb()+" <objeto(s)>\n");
-    return 0;
-  }
-
-  if (this_object()->query_property(LOADING_PROP))
-  {
-    notify_fail("Tu equipo todavía se está cargando, así que es un poco difícil "+
-      "que puedas dejar nada.\n");
     return 0;
   }
 
@@ -304,20 +317,20 @@ int drop_ob(string str)
   return 1;
 }
 
-int take(string str, varargs string verb, object *bing, string bing2, int blue)
+int do_take(string str, varargs string verb, object *bing, string bing2, int blue)
 {
   object *dest, *obs, *fail, *ret_a;
   mixed *ret;
   string s2, sh, str2, aux;
   int i, num, j, cap, perc, we, tot, max;
 
-  /* Added to get rid of one more auto_load bug..
-   * Baldrick, nov '94
-   */
-  if (this_object()->query_loading() ||
-      this_object()->query_property(LOADING_PROP))
+  i = 0; j = 0;
+
+  // Added to get rid of one more auto_load bug..
+  // Baldrick, nov '94
+  if (this_object()->query_loading() || this_object()->query_property(LOADING_PROP))
   {
-    notify_fail("Tienes que esperar a que todo tu equipo esté cargado.\n");
+    notify_fail(_LANG_HANDLE_WAIT_LOADING);
     return 0;
   }
 
@@ -329,17 +342,18 @@ int take(string str, varargs string verb, object *bing, string bing2, int blue)
 
   if (this_object()->query_dead())
   {
-    notify_fail("Tu estado etéreo tiene poco efecto en el mundo material.\n ");
+    notify_fail(_LANG_HANDLE_DEAD);
     return 0;
   }
 
   if (!strlen(str) && !verb)
   {
-    notify_fail("Sintaxis: "+query_verb()+" <objeto> [de <objeto>]\n");
+    notify_fail(_LANG_HANDLE_GET_SYNTAX);
     return 0;
   }
 
-  cap = (int)this_object()->query_max_weight()+1;
+  cap = (int)this_object()->query_max_weight() + 1;
+
   if (verb)
   {
     dest = bing;
@@ -351,10 +365,11 @@ int take(string str, varargs string verb, object *bing, string bing2, int blue)
     dest = ({ environment() });
 
     /* Caso 1 */
-    if (sscanf(str,"%smonedas de %s de %s",aux,str2,s2) == 3)
+    if (sscanf(str, "%smonedas de %s de %s", aux, str2, s2) == 3)
     {
       dest = find_match(s2, ({environment(), this_object() }), 1);
-      str = aux+"monedas de "+str2;
+      str = aux + "monedas de " + str2;
+      
       if (!sizeof(dest))
       {
         notify_fail("No encuentras ningún "+s2+" por aquí.\n");
@@ -379,66 +394,69 @@ int take(string str, varargs string verb, object *bing, string bing2, int blue)
       }
       else
       {
-        if (sscanf(str, "%s de %s", str, s2) == 2)
+        if (sscanf(str, "%s " + _LANG_HANDLE_GET_PREP + " %s", str, s2) == 2)
         {
           /* since we only allow one lvl anyway... (*/
           dest = find_match(s2, ({ environment(), this_object() }), 1);
           if (!sizeof(dest))
           {
-            notify_fail("No encuentras ningún "+s2+" por aquí.\n");
+            notify_fail(_LANG_HANDLE_GET_CANNOT);
             return 0;
           }
         }
       }
     }
+
     i = 0;
   }
 
-  for (;i<sizeof(dest);i++)
+  for (; i < sizeof(dest); i++)
   {
     if (dest[i]->cannot_get_stuff())
       continue;
 
     obs = find_match(str, dest[i], 1);
+
     ret = ({ ({ }), ({ }), ({ }), ({ }), ({ }), });
     ret_a = ({ });
     fail = ({ });
     tot = 0;
 
     for (j = 0; j < sizeof(obs); j++)
+    {
       if (obs[j]->move(this_object()) == MOVE_OK)
       {
         if ((perc = (we = (int)obs[j]->query_weight())*100/cap) >= 95)
           ret[4] += ({ obs[j] });
         else
           ret[perc/25] += ({ obs[j] });
+
         tot += we;
         ret_a += ({ obs[j] });
       }
       else if (!living(obs[j]))
+      {
         fail += ({ obs[j] });
+      }
+    }
 
     for (j = 0; j < sizeof(ret); j++)
+    {
       if (sizeof(ret[j]))
       {
         sh = query_multiple_short(ret[j], 1);
 
-        write(({ "Coges", "Coges con un poco de dificultad",
-          "Te las arreglas como puedes para coger",
-          "Te resulta muy difícil coger",
-          "Usas toda tu fuerza y a duras penas logras coger" })[j]+
-          " "+sh+" de "+(string)dest[i]->short()+".\n");
+        write(_LANG_HANDLE_GET_MESSAGES[j] + " " + sh + " " + 
+          _LANG_HANDLE_GET_PREP + " " + (string)dest[i]->short() + ".\n");
 
         if (!this_object()->query_hidden())
-          say((string)this_object()->query_cap_name()+" "+
-            ({ "coge", "coge con un poco de dificultad",
-            "se las arregla como puede para coger",
-            "tiene muchas dificultades para coger",
-            "usa toda su fuerza y a duras penas logra coger" })[j]+
-            " "+sh+" de "+(string)dest[i]->short()+".\n");
+          say((string)this_object()->query_cap_name() + " " + 
+            _LANG_HANDLE_GET_MESSAGES_OTHERS[j] + " " + sh + " " + 
+            _LANG_HANDLE_GET_PREP + " " + (string)dest[i]->short() + ".\n");
 
         num += sizeof(ret[j]);
       }
+    }
 
     if (tot)
     {
@@ -456,18 +474,19 @@ int take(string str, varargs string verb, object *bing, string bing2, int blue)
           tot = max;
         tot = (tot * 100)/max;
         this_object()->add_timed_property(PASSED_OUT_PROP,
-          "Aún estas ocupado con la carga.\n", tot/TIME_DIV);
+          _LANG_HANDLE_STILL, tot/TIME_DIV);
       }
     }
 
     if (sizeof(fail))
-      write("No puedes "+query_verb()+" "+query_multiple_short(fail, 1)+".\n");
+      write(_LANG_HANDLE_FAIL);
+
     num += sizeof(fail);
   }
 
   if (!num)
   {
-    notify_fail("Nada que "+query_verb()+".\n");
+    notify_fail(_LANG_HANDLE_GET_NOTHING);
     return 0;
   }
 
@@ -521,11 +540,11 @@ void get_put_interupt(int left, mixed arg) {
   case "dar" :
   case "poner":
   case "put" :
-    give(0, arg[2], per, thing, j+1);
+    do_give(0, arg[2], per, thing, j+1);
     break;
   case "get" :
   case "coger":
-    take(0, arg[2], per, thing, j+1);
+    do_take(0, arg[2], per, thing, j+1);
     break;
   }
   }
