d302dd6e47f5ae01833e83a16da019fa5654cc24
neverbot <ivan@neverbot.com>
2021-01-02 00:28:45 +0100

    fix: file with different encoding

--- mudlib/lib/player/.lang.es.h
+++ mudlib/lib/player/.lang.es.h
@@ -1,27 +1,27 @@
 
 // events.c
 
-#define _LANG_EVENTS_SOMEBODY_ARRIVES "Notas que alguien llega a tu posiciÃ³n.\n"
-#define _LANG_EVENTS_SOMEBODY_LEAVES  "Notas marcharse a uno de los que estaban aquÃ­.\n"
+#define _LANG_EVENTS_SOMEBODY_ARRIVES "Notas que alguien llega a tu posición.\n"
+#define _LANG_EVENTS_SOMEBODY_LEAVES  "Notas marcharse a uno de los que estaban aquí.\n"
 
 // start.c
 
 
 // quit.c
 
-#define _LANG_QUIT_LOADING "TodavÃ­a se esta cargando tu equipo, espera a que termine el proceso.\n"
+#define _LANG_QUIT_LOADING "Todavía se esta cargando tu equipo, espera a que termine el proceso.\n"
 #define _LANG_QUIT_EXITING "Saliendo...\n"
 #define _LANG_QUIT_ENV_MSG this_object()->query_cap_name()+ \
-            " busca un lugar cÃ³modo para salir del juego.\n"
+            " busca un lugar cómodo para salir del juego.\n"
 #define _LANG_QUIT_STOP_COMBAT_FIRST "Debes terminar primero tus combates.\n"+ \
         "Escribe '%^BOLD%^detener combates%^RESET%^' para terminarlos lo antes posible.\n"
-#define _LANG_QUIT_STOP_MOVING_FIRST "Si no paras de moverte serÃ¡ imposible buscar un buen lugar "+ \
+#define _LANG_QUIT_STOP_MOVING_FIRST "Si no paras de moverte será imposible buscar un buen lugar "+ \
         "para salir del juego.\n"
-#define _LANG_QUIT_BEEN_MOVED "En tu localizaciÃ³n previa no es posible salir, "+ \
-        "has sido movido a la mÃ¡s cercana (vuelve a intentarlo aquÃ­ si aÃºn "+ \
+#define _LANG_QUIT_BEEN_MOVED "En tu localización previa no es posible salir, "+ \
+        "has sido movido a la más cercana (vuelve a intentarlo aquí si aún "+ \
         "deseas desconectar).\n"
 #define _LANG_QUIT_DROP_ITEM "Dejas tu "+ob[i]->short()+".\n"
-#define _LANG_QUIT_THANKS "Gracias por jugar, Â¡hasta la prÃ³xima!\n"
+#define _LANG_QUIT_THANKS "Gracias por jugar, ¡hasta la próxima!\n"
 #define _LANG_QUIT_INFORM_CODERS this_object()->query_cap_name()+" sale de "+mud_name()
 #define _LANG_QUIT_INFORM_PLAYERS this_object()->query_cap_name()+" sale de "+mud_name()
 #define _LANG_QUIT_ENV_QUIT this_object()->query_cap_name()+" sale de "+mud_name()+".\n"
