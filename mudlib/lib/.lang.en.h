ed02142859891d260ba87d2346c6873c7f084aad
neverbot <ivan@neverbot.com>
2021-01-11 23:14:59 +0100

    fix: extra line removed

--- mudlib/lib/.lang.en.h
+++ mudlib/lib/.lang.en.h
@@ -1,7 +1,7 @@
 
 // user.c
 
-#define _LANG_DISCONNECTED "\nDisconnected.\n"
+#define _LANG_DISCONNECTED "Disconnected.\n"
 #define _LANG_LAST_CONNECTION "Your last connection was from '"+last_on_from+"'.\n"
 #define _LANG_USER_COMMAND_TOO_LONG "Command too long - processing anyway.\n"
 
