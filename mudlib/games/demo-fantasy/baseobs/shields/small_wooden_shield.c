8b7825962b5140ef800b0639318d3a7495aaed2f
neverbot <ivan@neverbot.com>
2023-02-22 19:18:40 +0100

    fix: alias and plural for small shield

--- mudlib/games/demo-fantasy/baseobs/shields/small_wooden_shield.c
+++ mudlib/games/demo-fantasy/baseobs/shields/small_wooden_shield.c
@@ -7,11 +7,14 @@ void setup()
 { 
   set_base_shield(SMALL_WOODEN_SHIELD);
 
+  add_alias(SMALL_WOODEN_SHIELD);
+
   set_name(_LANG_BASEOBS_SMALL_WOODEN_SHIELD);
   set_short(capitalize(_LANG_BASEOBS_SMALL_WOODEN_SHIELD));
  
   set_main_plural(capitalize(pluralize(_LANG_BASEOBS_SMALL_WOODEN_SHIELD)));
   add_plural(pluralize(_LANG_BASEOBS_SMALL_WOODEN_SHIELD));
+  add_plural(pluralize(SMALL_WOODEN_SHIELD));
 
   set_long(_LANG_BASEOBS_SMALL_WOODEN_SHIELD_LONG);
 } 
