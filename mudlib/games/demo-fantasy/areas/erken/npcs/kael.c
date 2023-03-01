faf231fcaa41f11f341c2c685ff459f09c149cb4
neverbot <ivan@neverbot.com>
2023-03-01 14:59:40 +0100

    fix(demo-fantasy): minor fix with kael

--- mudlib/games/demo-fantasy/areas/erken/npcs/kael.c
+++ mudlib/games/demo-fantasy/areas/erken/npcs/kael.c
@@ -31,6 +31,7 @@ void setup()
 
   add_clone(BASEOBS + "weapons/dagger", 1);
   add_clone(BASEOBS + "armours/leather", 1);
+  init_equip();
 
   // set_skill("dodge", 100);
   // set_skill("knife", 100);
