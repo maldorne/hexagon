5514e33d2a7298e76eb082a8e9a279b59b4e016f
neverbot <ivan@neverbot.com>
2024-12-11 20:08:58 +0100

    fix: shutdown cmd

--- mudlib/lib/cmds/admin/shutdown.c
+++ mudlib/lib/cmds/admin/shutdown.c
@@ -28,7 +28,7 @@ static int cmd(string arg, object me, string verb)
      return 1;
   }
   
-  if (!MASTER->high_programmer( geteuid(me) ) )
+  if (!SECURE_OB->high_programmer( geteuid(me) ) )
   {
      upt = uptime();
      if ((upt/(60*60)%24) < 3) 
@@ -39,11 +39,11 @@ static int cmd(string arg, object me, string verb)
   }
   
   sscanf(arg, "%d %s", tim, arg);
-  if (tim < 10 && !MASTER->high_programmer(geteuid(me)))
+  if (tim < 10 && !SECURE_OB->high_programmer(geteuid(me)))
     tim = 10;
 
   /* 
-  if (str[0..2] == "now" && MASTER->high_programmer(geteuid(me)))
+  if (str[0..2] == "now" && SECURE_OB->high_programmer(geteuid(me)))
     tim = 0;
   */
   /* Dejamos la comprobacion del tiempo para el /obj/shut.c, neverbot
@@ -53,7 +53,7 @@ static int cmd(string arg, object me, string verb)
   */
 
   // Cancelar shutdown añadido por neverbot, 23/4/2003
-  if ((arg == "off") && MASTER->high_programmer(geteuid(me)))
+  if ((arg == "off") && SECURE_OB->high_programmer(geteuid(me)))
   {
     write("Shutdown cancelled.\n");
     handler("shutdown")->dest_me();
