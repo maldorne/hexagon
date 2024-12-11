cbfcadce34a8b1245e30fd6f0ced43882e967609
neverbot <ivan@neverbot.com>
2024-12-11 19:11:51 +0100

    feat: improve areas cmd

--- mudlib/lib/cmds/admin/areas.c
+++ mudlib/lib/cmds/admin/areas.c
@@ -7,8 +7,10 @@ inherit CMD_BASE;
 void setup()
 {
   set_aliases(({ "areas" }));
-  set_usage("areas");
-  set_help("List areas loaded in the location system.");
+  set_usage("areas [locations] [connections]");
+  set_help("List areas loaded in the location system.\n" + 
+           "If 'locations' is specified, it will also list the locations of every area.\n" + 
+           "If 'connections' is specified, it will also list their connections to other areas.\n");
 }
 
 static int cmd(string str, object me, string verb) 
@@ -16,6 +18,9 @@ static int cmd(string str, object me, string verb)
   mapping areas;
   string * keys;
   int i, j;
+  string * pieces;
+  int show_locations, show_connections;
+  string ret;
 
   areas = load_object(AREA_HANDLER)->query_loaded_areas();
 
@@ -25,22 +30,57 @@ static int cmd(string str, object me, string verb)
     return 1;
   }
 
-  write("Areas loaded:\n");
+  show_locations = FALSE;
+  show_connections = FALSE;
+  pieces = explode(str, " ");
+
+  if (member_array("locations", pieces) != -1)
+    show_locations = TRUE;
+  if (member_array("connections", pieces) != -1)
+    show_connections = TRUE;
+
+  ret = "Areas loaded:\n";
   keys = keys(areas);
 
   for (i = 0; i < sizeof(keys); i++)
   {
     object * locations;
+    mapping connections;
+    string * connections_keys;
 
-    write(" - " + keys[i] + "\n");
+    ret += " - %^GREEN%^" + keys[i] + "%^RESET%^\n";
 
-    locations = areas[keys[i]]->query_loaded_locations();
-
-    for (j = 0; j < sizeof(locations); j++)
+    if (show_locations)
     {
-      write("   - " + locations[j]->query_file_name() + "\n");
+      ret += "\n  Locations:\n\n";
+      locations = areas[keys[i]]->query_loaded_locations();
+
+      for (j = 0; j < sizeof(locations); j++)
+      {
+        ret += "   - " + locations[j]->query_file_name() + "\n";
+      }
+
+      ret += "\n";
+    }
+
+    if (show_connections)
+    {
+      ret += "\n  Connections:\n\n";
+
+      connections = areas[keys[i]]->query_connections();
+      connections_keys = keys(connections);
+
+      for (j = 0; j < sizeof(connections_keys); j++)
+      {
+        ret += "   - " + connections_keys[j] + "\n     %^CYAN%^" + 
+              connections[connections_keys[j]][0] + "%^RESET%^ : " + 
+              connections[connections_keys[j]][1] + "\n";
+      }
+
+      ret += "\n";
     }
   }
 
+  write(ret);
   return 1;
 }
