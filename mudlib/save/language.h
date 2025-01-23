e02b22a1cbad398b39fad1eb2767927c76d280da
neverbot <ivan@neverbot.com>
2025-01-23 19:41:44 +0100

    fix: when including files already ending in .en/.es/etc

--- mudlib/save/language.h
+++ mudlib/save/language.h
@@ -6,13 +6,18 @@
  * This is the only file that you will need to edit to change to 
  * mud language.
  *
- * Currently supported languages: es, en
+ * Currently supported languages: see SUPPORTED_LANGUAGES
  *
  * Change the language both in GLOBAL_COMPILE_LANG and the file extension of 
- * the translation include files below.
+ * the translation include files below, using one of the languages
+ * indicated in SUPPORTED_LANGUAGES
  *
  */
 
+// do not change this line for translations purposes
+#define SUPPORTED_LANGUAGES ({ "en", "es" }) 
+
+// change below this comment
 #define GLOBAL_COMPILE_LANG "en"
 
 #include <translations/common.en.h>
