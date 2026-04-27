1c88a95489fc2e9b9af77273906e4e3d5da6ee74
neverbot <ivan@neverbot.com>
2026-04-27 18:51:18 +0200

    fix(coder/tail): use fixed-size tail read instead of byte-by-byte backwards loop

--- mudlib/lib/cmds/coder/tail.c
+++ mudlib/lib/cmds/coder/tail.c
@@ -1,30 +1,28 @@
 /** coded by Raskolnikov July 1996 **/
 /** Vaeleth... tail more flexible **/
+/** rewrite to fixed-size tail read, neverbot 04/2026 **/
 
 #include <mud/cmd.h>
 
 inherit CMD_BASE;
+
+#define DEFAULT_LINES  10
+#define TAIL_BYTES     16384  /* upper bound on bytes read from the file end */
 
 void setup()
 {
   set_aliases(({ "tail" }));
   set_usage("tail [-<number of lines>] <files>");
   set_help("Shows the last lines of a file.\n" +
-    "A default of 10 lines is returned if range is not specified.\n\n" +
+    "A default of " + DEFAULT_LINES + " lines is returned if range is not specified.\n\n" +
     "Ex. tail -20 /lib/room.c\n" +
     "will return the last 20 lines of the file room.c.");
 }
 
 static int cmd(string str, object me, string verb)
 {
-  string file, *filename, text;
-  int range, offset, lines;
-  string content, err;
-
-  content = "";
-  text = "";
-  offset = -1;
-  lines = 0;
+  string file, *filename, *lines, content, err;
+  int range, fsize, want, start;
 
   if (!strlen(str))
   {
@@ -33,36 +31,56 @@ static int cmd(string str, object me, string verb)
   }
 
   if (sscanf(str, "-%d %s", range, file) != 2)
-  {
     file = str;
-  }
-
-  filename = get_files(file);
-
-  if (!sizeof(filename))
-  {
-    notify_fail("That file does not exist.\n");
-    return 0;
-  }
 
   if (range < 0)
   {
     notify_fail("Invalid range: " + range + ".\n");
     return 0;
   }
-
   if (!range)
-    range = 10;
+    range = DEFAULT_LINES;
 
-  while (!err && lines <= range)
+  filename = get_files(file);
+  if (!sizeof(filename))
   {
-    text = content + text;
-    err = catch(content = read_file(filename[0], offset, 1));
-    offset--;
-    if (content == "\n")
-      lines++;
+    notify_fail("That file does not exist.\n");
+    return 0;
   }
 
-  write(text);
+  fsize = file_size(filename[0]);
+  if (fsize < 0)
+  {
+    notify_fail("Cannot read " + filename[0] + ".\n");
+    return 0;
+  }
+  if (fsize == 0)
+  {
+    write("");
+    return 1;
+  }
+
+  /* read at most TAIL_BYTES from the end of the file */
+  want  = (fsize < TAIL_BYTES) ? fsize : TAIL_BYTES;
+  start = fsize - want;
+
+  err = catch(content = read_file(filename[0], start, want));
+  if (err || !content)
+  {
+    notify_fail("Could not read " + filename[0] + ".\n");
+    return 0;
+  }
+
+  lines = explode(content, "\n");
+
+  /* if we didn't start at byte 0, the first element is a partial line; drop it */
+  if (start != 0 && sizeof(lines) > 1)
+    lines = lines[1..];
+
+  /* keep only the last `range` lines */
+  if (sizeof(lines) > range)
+    lines = lines[sizeof(lines) - range ..];
+
+  write(implode(lines, "\n") + "\n");
   return 1;
 }
