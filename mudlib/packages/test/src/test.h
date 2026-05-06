48f02ea7a2ea5510d2f2951e1ddcecf5b0887450
neverbot <ivan@neverbot.com>
2026-05-06 02:17:35 +0200

    fix: wrap ASSERT macro in do-while(0) so it expands to a single statement

--- mudlib/packages/test/src/test.h
+++ mudlib/packages/test/src/test.h
@@ -9,8 +9,12 @@
 #define START_ASSERT() find_object(TEST_H)->start_assert()
 #define END_ASSERT() find_object(TEST_H)->end_assert()
 
-#define ASSERT(x) START_ASSERT();if (!(x)) { mixed t;t=call_trace();write("%^BOLD%^RED%^"+file_name(this_object()) + ": Check failed on line "+t[sizeof(t)-1][3]+".%^RESET%^\n"); }END_ASSERT();
-#define ASSERT2(x, r) if (!(x)) { write("%^BOLD%^RED%^"+file_name(this_object()) + ":" + r + ", Check failed.%^RESET%^\n"); }
+// wrapped in do {...} while (0) so the macro expands to a single
+// statement; otherwise using ASSERT as the body of a brace-less for/if
+// would split the if/END_ASSERT outside the loop and cause spurious
+// "Array index out of range" errors.
+#define ASSERT(x) do { START_ASSERT();if (!(x)) { mixed t;t=call_trace();write("%^BOLD%^RED%^"+file_name(this_object()) + ": Check failed on line "+t[sizeof(t)-1][3]+".%^RESET%^\n"); }END_ASSERT(); } while (0)
+#define ASSERT2(x, r) do { if (!(x)) { write("%^BOLD%^RED%^"+file_name(this_object()) + ":" + r + ", Check failed.%^RESET%^\n"); } } while (0)
 
 #define ADD_LIVING(x) set_living_name(x)
 #define REMOVE_LIVING() LIVING_HANDLER->remove_living(this_object())
