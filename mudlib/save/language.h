be9f1659f2a84c864798a1540f7719fed445c814
neverbot <ivan@neverbot.com>
2026-07-13 20:18:53 +0200

    chore: minor change

--- mudlib/save/language.h
+++ mudlib/save/language.h
@@ -3,19 +3,19 @@
 /*
  * Language
  *
- * This is the only file that you will need to edit to change to 
+ * This is the only file that you will need to edit to change to
  * mud language.
  *
  * Currently supported languages: see SUPPORTED_LANGUAGES
  *
- * Change the language both in GLOBAL_COMPILE_LANG and the file extension of 
+ * Change the language both in GLOBAL_COMPILE_LANG and the file extension of
  * the translation include files below, using one of the languages
  * indicated in SUPPORTED_LANGUAGES
  *
  */
 
 // do not change this line for translations purposes
-#define SUPPORTED_LANGUAGES ({ "en", "es" }) 
+#define SUPPORTED_LANGUAGES ({ "en", "es" })
 
 // change below this comment
 #define GLOBAL_COMPILE_LANG "en"
